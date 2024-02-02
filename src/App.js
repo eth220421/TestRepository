76b72de86bb93cf63941871c78a921e8755e7406
eth220421 <eth220421@gmail.com>
2024-02-02 13:30:33 +0900

    test main branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          main branch
+          test main branch
         </a>
       </header>
     </div>
