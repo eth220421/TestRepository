acca90b28d6c7569cc88828698a640dc4dcd1c8a
eth220421 <eth220421@gmail.com>
2024-02-02 13:47:10 +0900

    24.02.02 13:47

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          test sub branch
+          Here is main branch
         </a>
       </header>
     </div>
